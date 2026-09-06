748752180b05ed55a3de0c20abe26b4d777c8d60
RadenPasya <andikapasya70@gmail.com>
2026-09-06 23:22:35 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,10 @@
+import sys
+import os
+
+# Menambahkan root directory ke sys.path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import app
 
-# Vercel membutuhkan variabel 'app' ini untuk fungsi Serverless
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
